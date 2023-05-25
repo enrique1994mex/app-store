32d00de5e91b9ad35613ad2f0e8657fd724bdbf7
Enrique Sanchez Sandoval <enrique.antonio1029@gmail.com>
2023-05-24 21:36:02 -0600

    initial configuration

---
+++ src/app/MyAccount/page.tsx
@@ -0,0 +1,5 @@
+const MyAccount = () => {
+	return <div className='bg-red-100'>MyAccount</div>
+}
+
+export default MyAccount
---
+++ src/app/MyOrders/page.tsx
@@ -0,0 +1,5 @@
+const MyOrders = () => {
+	return <div className='bg-red-100'>MyOrders</div>
+}
+
+export default MyOrders
